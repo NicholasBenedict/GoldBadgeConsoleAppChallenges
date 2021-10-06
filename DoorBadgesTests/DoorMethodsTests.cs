e9a03938bd8b502f42d6665256d33f05c592346e
Nicholas Benedict <nicholasdbenedict@gmail.com>
2021-10-06 09:23:12 -0400

    unit testing for repos added

---
+++ DoorBadgesTests/DoorMethodsTests.cs
@@ -0,0 +1,85 @@
+﻿using DoorBadges;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace DoorBadgesTests
+{
+    [TestClass]
+    public class DoorMethodsTests
+    {
+        [TestMethod]
+        public void AddBadgeToRepo_ShouldReturnCorrectBoolValueIfBadgeAddedToRepo()
+        {
+            BadgeClass badge = new BadgeClass();
+            BadgesRepo repo = new BadgesRepo();
+
+            bool success = repo.AddContentToDirectory(badge);
+
+            Assert.IsTrue(success);
+        }
+
+        [TestMethod]
+        public void GetBadge_ShouldReturnCorrectBoolValueIfTheBadgesWereGottenFromRepo()
+        {
+            int badgeNumber = 12345;
+            List<string> doorNumbers = new List<string>();
+
+            doorNumbers.Add("A6");
+            doorNumbers.Add("A7");
+            doorNumbers.Add("A8");
+
+            BadgeClass badge = new BadgeClass(badgeNumber, doorNumbers);
+            BadgesRepo badgesRepo = new BadgesRepo();
+
+            badgesRepo.AddContentToDirectory(badge);
+
+            Dictionary<int, List<string>> badgesList = badgesRepo.GetContents();
+
+            bool success = badgesList.ContainsKey(badgeNumber);
+
+            Assert.IsTrue(success);
+        }
+        [TestMethod]
+        public void RemoveRoomFromBadge_ShouldReturnCorrectValueIfRoomWasRemovedFromBadge()
+        {
+            int badgeNumber = 12345;
+            List<string> doorNumbers = new List<string>();
+            string doorNum = "A6";
+            doorNumbers.Add("A6");
+            doorNumbers.Add("A7");
+            doorNumbers.Add("A8");
+
+            BadgeClass badge = new BadgeClass(badgeNumber, doorNumbers);
+            BadgesRepo badgeRepo = new BadgesRepo();
+            BadgesRepo badgeRepo2 = new BadgesRepo();
+
+            badgeRepo2.AddContentToDirectory(badge);
+            badgeRepo.AddContentToDirectory(badge);
+
+            badgeRepo.RemoveRoomFromBadge(doorNumbers, doorNum, badgeNumber);
+
+            Assert.AreNotEqual(badgeRepo2, badgeRepo);
+        }
+        [TestMethod]
+        public void AddRoomToBadge_ShouldReturnCorrectValueIfRoomWasAddedToBadge()
+        {
+            int badgeNumber = 12345;
+            List<string> doorNumbers = new List<string>();
+            string doorNum = "A6";
+            doorNumbers.Add("A7");
+            doorNumbers.Add("A8");
+
+            BadgeClass badge = new BadgeClass(badgeNumber, doorNumbers);
+            BadgesRepo badgeRepo = new BadgesRepo();
+            BadgesRepo badgeRepo2 = new BadgesRepo();
+
+            badgeRepo2.AddContentToDirectory(badge);
+            badgeRepo.AddContentToDirectory(badge);
+
+            badgeRepo.AddRoomToBadge(doorNum, badgeNumber, doorNumbers);
+
+            Assert.AreNotEqual(badgeRepo2, badgeRepo);
+        }
+    }
+}
---
+++ DoorBadgesTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DoorBadgesTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DoorBadgesTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("03b64329-9760-452b-bfd2-ccb635103edd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
